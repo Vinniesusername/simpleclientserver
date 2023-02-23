b526a8390047990ee4d3ec8e9d60736e13b17659
Vinnie <vprestianni@luc.edu>
2023-02-22 20:54:09 -0600

    THIS IS FINE

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,19 @@
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.*;
 import java.io.*;
-
+import java.security.KeyStore;
 
 
 public class Client  // this class deals with all socket requests to Server.java
 {
     private static Client handler = null; // one object following singleton pattern.
-    public SSLSocketFactory sslf = (SSLSocketFactory) SSLSocketFactory.getDefault();
     public SSLSocket cs;
     public BufferedReader in = null;
     public PrintWriter out = null;
+
+    public String ksPath = "D:\\SPS\\keystore\\spsclient";
+
+    final String keypass = "testkey"; //hard coded to save time obv dont do this for real software
+    final String kspass = "testclient";
     private static final String[] protocols = new String[]{"TLSv1.3"};
 
     private static final String[] ciphers = new String[]{"TLS_AES_256_GCM_SHA384", "TLS_AES_128_GCM_SHA256"};
@@ -30,7 +33,7 @@ public class Client  // this class deals with all socket requests to Server.java
         try
         {
             handler.connect();
-            //System.out.println(handler.askForMagicNumber());
+            System.out.println(handler.askForMagicNumber());
         }
         catch (Exception e)
         {
@@ -55,15 +58,29 @@ public class Client  // this class deals with all socket requests to Server.java
         boolean connected = false;
         try
         {
-            handler.cs = (SSLSocket)handler.sslf.createSocket("localhost", 4422);
-            handler.cs.setEnabledProtocols(protocols);
+            //key store/manager
+            KeyManagerFactory keyfact = KeyManagerFactory.getInstance("PKIX");
+            KeyStore ks = KeyStore.getInstance("pkcs12");
+            ks.load(new FileInputStream(ksPath), kspass.toCharArray());
+            keyfact.init(ks, keypass.toCharArray());
+            //trust store
+            TrustManagerFactory trstfact = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trstfact.init(ks);
+
+            //socket
+            SSLContext context = SSLContext.getInstance(protocols[0]);
+            context.init(keyfact.getKeyManagers(), trstfact.getTrustManagers(), null);
+            SSLSocketFactory sockfact = context.getSocketFactory();
+
+
+            handler.cs = (SSLSocket) sockfact.createSocket("localhost", 4422);
             //enable only the protocols and ciphers that i want used
-            handler.cs.setEnabledProtocols(protocols);
-            handler.cs.setEnabledCipherSuites(ciphers);
+            //handler.cs.setEnabledProtocols(protocols);
+            //handler.cs.setEnabledCipherSuites(ciphers);
             //set input and output streams of socket
             handler.out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(handler.cs.getOutputStream())));
             handler.in = new BufferedReader(new InputStreamReader(handler.cs.getInputStream()));
-            System.out.println(handler.out.checkError());
+
 
             // write to socket to test it
             handler.out.println("TESTC");
@@ -72,7 +89,7 @@ public class Client  // this class deals with all socket requests to Server.java
 
 
             //check for error
-            if(handler.out.checkError() == false)
+            if(handler.out.checkError())
             {
                 throw new Exception("java.io.printwriter error");
             }
@@ -86,10 +103,6 @@ public class Client  // this class deals with all socket requests to Server.java
         catch (Exception e)
         {
             e.printStackTrace();
-            //if any exception is thrown then make sure the socket is clear
-            int s = disconnect();
-            if(s != 0)
-                System.out.println("disconnect issuse");
             status = -2;
         }
         finally
@@ -107,6 +120,15 @@ public class Client  // this class deals with all socket requests to Server.java
 
     public int askForMagicNumber()
     {
+        //test code remove later
+        String[] temp = handler.cs.getEnabledCipherSuites();
+        String[] temp2 = handler.cs.getEnabledProtocols();
+        for(int i =0; i < temp.length; i++)
+            System.out.println(temp[i]);
+        for(int i =0; i < temp2.length; i++)
+            System.out.println(temp2[i]);
+
+        //end test code
         handler.out.flush();
         handler.out.println();
         String q = "58;null;null;null;null";
@@ -115,9 +137,11 @@ public class Client  // this class deals with all socket requests to Server.java
 
         try
         {
-            String temp =  handler.in.readLine();
-            String[] parts = temp.split(";", 5);
-            r += Integer.parseInt(parts[0]);
+            String temp0 =  handler.in.readLine();
+            if(temp0 != null) {
+                String[] parts = temp0.split(";", 5);
+                r += Integer.parseInt(parts[0]);
+            }
         }
         catch (Exception e)
         {
@@ -125,58 +149,6 @@ public class Client  // this class deals with all socket requests to Server.java
         }
         return r;
     }
-
-    public void listenToServer()
-    {
-        try
-        {
-            handler.cs = (SSLSocket) handler.sslf.createSocket("localhost", 4422);
-            handler.cs.setEnabledProtocols(protocols);
-            //enable only the protocols and ciphers that i want used
-            handler.cs.setEnabledProtocols(protocols);
-            handler.cs.setEnabledCipherSuites(ciphers);
-            //set input and output streams of socket
-            handler.out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(handler.cs.getOutputStream())));
-            handler.in = new BufferedReader(new InputStreamReader(handler.cs.getInputStream()));
-            System.out.println(handler.out.checkError());
-
-            while(true)
-            {
-                while(handler.in.ready())
-                {
-                    String q = handler.in.readLine();
-                    System.out.println(q);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-
-
-    }
-
-
-    public int disconnect()
-
-    {
-        int status = 0;
-        if(handler.cs == null)
-            return status;
-        try
-        {
-            handler.cs.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            status = -1;
-        }
-        return status;
-    }
-
 
 
 }
